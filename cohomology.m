c3dfc9b026d148595c2e40eecf2cb570b877fdf7
daniellbassett <dlbassett.pr@gmail.com>
2025-05-20 16:33:18 +0100

    Take representative facets under cell stabiliser

--- cohomology.m
+++ cohomology.m
@@ -4,6 +4,7 @@
 TODO: write description
 */
 
+import "symmetric_space.m" : barycentre;
 import "module.m" : invariants, action_permutation, action_matrix;
 
 function coboundaryMap(M, complex, d) //coboundary map in degree d: from dimension d-1 cells to dimension d cells
@@ -12,6 +13,7 @@ function coboundaryMap(M, complex, d) //coboundary map in degree d: from dimensi
 	total_matrix := <>;
 	
 	for i in [1..#complex`cell_reps[codim]] do //a higher-dim cell rep, sigma
+		print "\t\tcoboundary: Cell", i, "of", #complex`cell_reps[codim];
 		M_sigma := invariants(M, complex`cell_rep_stabilisers[codim][i], complex`orientation_characters[codim][i]);
 		coord_space := VectorSpace(M`base_field, Dimension(M_sigma));
 		
@@ -19,7 +21,28 @@ function coboundaryMap(M, complex, d) //coboundary map in degree d: from dimensi
 		
 		sigma_matrix := <RMatrixSpace(M`base_field, Dimension(M_taus[j]), Dimension(M_sigma)) ! 0 : j in [1..#complex`cell_reps[codim+1]]>;
 		
+		facet_barycentres := [barycentre(facet) : facet in complex`facets[codim][i]];
 		for j in [1..#complex`facets[codim][i]] do //a facet of sigma, tau
+			//check if facet is equivalent under Gamma_sigma to a previous facet
+			if #complex`cell_rep_stabilisers[codim][i] gt 0 then
+				equiv := false;
+				G := MatrixGroup<NumberOfColumns(complex`cell_rep_stabilisers[codim][i][1]), Parent(complex`cell_rep_stabilisers[codim][i][1][1,1]) | complex`cell_rep_stabilisers[codim][i]>;
+				for g in G do
+					gIndex := Index(facet_barycentres, facet_barycentres[j] * g);
+					
+					if gIndex lt j then
+						equiv := true;
+						break;
+					end if;
+				end for;
+				
+				if equiv then
+					print"\t\t\tcoboundary: Facet", j, "of", #complex`facets[codim][i], "(skipping)";
+					continue;
+				end if;
+			end if;
+			
+			
 			if #complex`facet_cell_stabiliser_cosets[codim][i][j] eq 1 then //facet stabiliser contains cell stabiliser
 				index := complex`facet_equiv_indices[codim][i][j];
 				if Dimension(M_taus[index]) eq 0 then
@@ -29,7 +52,7 @@ function coboundaryMap(M, complex, d) //coboundary map in degree d: from dimensi
 				gamma := complex`facet_equiv_witnesses[codim][i][j];
 				
 				if gamma eq 1 then
-					//print "standard";
+					print "\t\t\tcoboundary: Facet", j, "of", #complex`facets[codim][i], "(standard)";
 					rows := [];
 					
 					for k in [1..Dimension(M_taus[index])] do
@@ -42,8 +65,7 @@ function coboundaryMap(M, complex, d) //coboundary map in degree d: from dimensi
 					//	print orientation_factor * VerticalJoin(rows);
 					//end if;
 				else
-					//print "used in degree", d;
-					//print "permutation";
+					//print "\t\t\tcoboundary: Facet", j, "of", #complex`facets[codim][i], "(permutation)";
 					permutation := action_permutation(M, gamma); //for some reason gamma^-1 makes it not crash but we still get negative answers
 					rows := [];
 					
@@ -59,8 +81,7 @@ function coboundaryMap(M, complex, d) //coboundary map in degree d: from dimensi
 				end if;
 			else
 				//corestriction map: must sum over cosets
-				//print "cores used in degree", d;
-				//print "corestriction";
+				print "\t\t\tcoboundary: Facet", j, "of", #complex`facets[codim][i], "(corestriction)";
 				index := complex`facet_equiv_indices[codim][i][j];
 				if Dimension(M_taus[index]) eq 0 then
 					continue;
@@ -69,8 +90,9 @@ function coboundaryMap(M, complex, d) //coboundary map in degree d: from dimensi
 				gamma := complex`facet_equiv_witnesses[codim][i][j];
 				
 				M_matrix := MatrixRing(M`base_field, #M`cosets) ! 0;
-				for coset in complex`facet_cell_stabiliser_cosets[codim][i][j] do
-					M_matrix +:= action_matrix(M, gamma * coset);
+				for k in [1..#complex`facet_cell_stabiliser_cosets[codim][i][j]] do
+					//print "\t\t\t\tcorestriction: Coset", k, "of", #complex`facet_cell_stabiliser_cosets[codim][i][j];
+					M_matrix +:= complex`coset_character[codim][i][j][k] * action_matrix(M, gamma * complex`facet_cell_stabiliser_cosets[codim][i][j][k]);
 				end for;
 				
 				orientation_factor := complex`facet_equiv_orientation[codim][i][j] * complex`facet_cell_orientation[codim][i][j];
@@ -112,9 +134,10 @@ function cohomology(M, complex, degrees, with_torsion)
 	coboundary_rows := [];
 	coboundary_columns := [];
 	coboundary_divisors := [];
-	coboundary_maps := [**];
+	//coboundary_maps := [**];
 
 	for d in required_coboundary_degrees do
+		print "\tcohomology: coboundary degree", d;
 		codim := #complex`cell_reps-d;
 		if #complex`cell_reps[codim] eq 0 or #complex`cell_reps[codim+1] eq 0 then
 			Append(~coboundary_ranks, 0);
@@ -141,7 +164,7 @@ function cohomology(M, complex, degrees, with_torsion)
 			Append(~coboundary_rows, NumberOfRows(map));
 			Append(~coboundary_columns, NumberOfColumns(map));
 			
-			Append(~coboundary_maps, map);
+			//Append(~coboundary_maps, map);
 			//print map;
 			
 			if with_torsion then
@@ -180,6 +203,91 @@ function cohomology(M, complex, degrees, with_torsion)
 			else
 				Append(~data, coboundary_columns[#coboundary_columns] - coboundary_ranks[#coboundary_ranks]);
 				//print "ker:", coboundary_columns[#coboundary_columns], "im:", coboundary_ranks[#coboundary_ranks];
+			end if;
+		end for;
+	end if;
+
+	return data;
+end function;
+
+function homology(M, complex, degrees, with_torsion)
+	required_boundary_degrees := [];
+	for d in degrees do
+		if d gt 0 then
+			Append(~required_boundary_degrees, d);
+		end if;
+		
+		if d+1 lt #complex`cell_reps then
+			Append(~required_boundary_degrees, d+1);
+		end if;
+	end for;
+	
+	boundary_ranks := [];
+	boundary_nullities := [];
+	boundary_rows := [];
+	boundary_columns := [];
+	boundary_divisors := [];
+	
+	required_boundary_degrees := Sort(SetToSequence(SequenceToSet(required_boundary_degrees)));	
+	for d in required_boundary_degrees do
+		print "\thomology: boundary degree", d;
+		codim := #complex`cell_reps-d;
+		
+		if #complex`cell_reps[codim] eq 0 or #complex`cell_reps[codim+1] eq 0 then
+			Append(~boundary_ranks, 0);
+			
+			rows := 0;
+			columns := 0;
+			
+			for i in [1..#complex`cell_reps[codim]] do
+				rows +:= Dimension(invariants(M, complex`cell_rep_stabilisers[codim][i], complex`orientation_characters[codim][i]));
+			end for;
+			
+			for i in [1..#complex`cell_reps[codim+1]] do
+				columns +:= Dimension(invariants(M, complex`cell_rep_stabilisers[codim+1][i], complex`orientation_characters[codim+1][i]));
+			end for;
+			
+			Append(~boundary_rows, rows);
+			Append(~boundary_columns, columns);
+			
+			Append(~boundary_divisors, [1]);
+		else
+			map := Transpose(coboundaryMap(M, complex, d));
+			
+			Append(~boundary_ranks, Rank(map));
+			Append(~boundary_rows, NumberOfRows(map));
+			Append(~boundary_columns, NumberOfColumns(map));
+			
+			if with_torsion then
+				map := ChangeRing(map, Integers());
+				Append(~boundary_divisors,  ElementaryDivisors(map));
+			end if;
+		end if;
+	end for;
+	
+	map := Integers() ! 0;
+	
+	data := [];
+	if with_torsion then
+		for d in degrees do
+			if d eq 0 then
+				Append(~data, [boundary_columns[1] - boundary_ranks[1], &*boundary_divisors[1]]);
+			elif d+1 ne #complex`cell_reps then
+				i := Index(required_boundary_degrees, d);
+				Append(~data, [boundary_rows[i] - boundary_ranks[i] - boundary_ranks[i+1], &*boundary_divisors[i+1]]);
+			else
+				Append(~data, [boundary_rows[#boundary_rows] - boundary_ranks[#boundary_ranks], 1]);
+			end if;
+		end for;
+	else
+		for d in degrees do
+			if d eq 0 then
+				Append(~data, boundary_columns[1] - boundary_ranks[1]);
+			elif d+1 ne #complex`cell_reps then
+				i := Index(required_boundary_degrees, d);
+				Append(~data, boundary_rows[i] - boundary_ranks[i] - boundary_ranks[i+1]);
+			else
+				Append(~data, boundary_rows[#boundary_rows] - boundary_ranks[#boundary_ranks]);
 			end if;
 		end for;
 	end if;
